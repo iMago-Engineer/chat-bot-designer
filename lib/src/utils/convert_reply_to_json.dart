ea3ae1d25dbf880cf15a3ed027456b87cad05ec3
KY-Ng <raionkurane@gmail.com>
2021-09-26 05:22:22 +0900

    some cleanup

--- lib/src/utils/convert_reply_to_json.dart
+++ lib/src/utils/convert_reply_to_json.dart
@@ -1,62 +1,74 @@
-import 'package:chat_bot_designer/src/models/choice.dart';
+import 'dart:convert';
 
+import '../models/choice.dart';
 import '../models/reply.dart';
 
-String convertReplyMapToJson(Map<Reply, Map<String, Reply>> edges) {
-  // algorithm
+String convertReplyMapToJson(Map<Reply, Map<String, Reply>> replyMap) {
   Map<String, dynamic> content = {};
-  content["start"] = _createOnePart(edges.keys.first, edges.keys.first.choices);
-  for (var edge in edges.entries) {
-    edge.key.choices.forEach((action) {
-      content["${edge.key.title}[${action.text}]"] = _createOnePart(
-          edge.value[action.text], edge.value[action.text]?.choices);
-    });
+
+  final firstReply = replyMap.keys.first;
+  content['start'] = _createOnePart(firstReply);
+
+  for (final entry in replyMap.entries) {
+    for (final choice in entry.key.choices) {
+      // User が返信できる時にだけ生成する
+      if (entry.value.containsKey(choice.text)) {
+        final partialContent = _createOnePart(entry.value[choice.text]);
+
+        // User が送ってきたものに対する返信が定義されている時だけ生成する
+        if (partialContent != null) {
+          final title = entry.key.title ?? '';
+
+          content['$title[${choice.text}]'] = partialContent;
+        }
+      }
+    }
   }
 
-  return content.toString();
+  return json.encode(content);
 }
 
 // 1つの塊
-List<Map<String, dynamic>>? _createOnePart(
-  Reply? reply,
-  List<Choice>? actions,
-) {
+List<Map<String, dynamic>>? _createOnePart(Reply? reply) {
   if (reply == null) return null;
 
-  if (reply.type == ReplyType.template && actions == null) return null;
+  switch (reply.type) {
+    case ReplyType.template:
+      if (reply.choices.isEmpty) return null;
 
-  if (reply.type == ReplyType.text && reply.text.isEmpty) return null;
-
-  if (reply.type == ReplyType.template) {
-    return [
-      {
-        "type": "template",
-        "altText": "this is a buttons template",
-        "template": {
-          "type": "buttons",
-          "title": reply.title,
-          "text": reply.text,
-          "actions": _createActionPart(reply.title, actions!)
+      return [
+        {
+          'type': 'template',
+          'altText': 'this is a button template',
+          'template': {
+            'type': 'buttons',
+            'title': reply.title,
+            'text': reply.text,
+            'actions': _createActionPart(reply),
+          },
         }
-      }
-    ];
-  } else if (reply.type == ReplyType.text) {
-    return [
-      {"type": "text", "text": reply.text}
-    ];
+      ];
+    case ReplyType.text:
+      if (reply.text.isEmpty) return null;
+      return [
+        {
+          'type': 'text',
+          'text': reply.text,
+        }
+      ];
+    default:
+      return null;
   }
 }
 
-List<dynamic> _createActionPart(String? title, List<Choice> actions) {
-  final formattedTitle = title ?? '';
+List<dynamic> _createActionPart(Reply reply) {
+  final formattedTitle = reply.title ?? '';
 
-  List<dynamic> choices = [];
-  for (var action in actions) {
-    choices.add({
-      "type": "message",
-      "label": action.text,
-      "text": "$formattedTitle[${action.text}]"
-    });
-  }
-  return choices;
+  return reply.choices.map((Choice choice) {
+    return {
+      'type': 'message',
+      'label': choice.text,
+      'text': '$formattedTitle[${choice.text}]'
+    };
+  }).toList();
 }
