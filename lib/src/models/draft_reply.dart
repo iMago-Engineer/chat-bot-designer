4f78eaf311569e47e1b65b5b2eb472c21eeff66f
KY-Ng <raionkurane@gmail.com>
2021-09-26 03:41:04 +0900

    update model

--- lib/src/models/draft_reply.dart
+++ lib/src/models/draft_reply.dart
@@ -1,27 +1,26 @@
-import 'package:chat_bot_designer/src/models/choice.dart';
 import 'package:chat_bot_designer/src/models/reply.dart';
 
 class DraftReply {
   final ReplyType type;
   final String? title;
   final String text;
-  final List<Choice>? choices;
+  final String trigger;
 
   DraftReply({
+    required this.trigger,
     required this.type,
     required this.title,
     required this.text,
-    required this.choices,
   });
 
   @override
   String toString() {
     return '''
     DraftReply: {
+      trigger: $trigger,
       type: $type,
       title: $title,
       text: $text,
-      choices: $choices,
     }
     ''';
   }
