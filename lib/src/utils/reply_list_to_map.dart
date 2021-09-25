b18a8b6bfb71fbb859709397db3672c2987e9ab1
KY-Ng <raionkurane@gmail.com>
2021-09-26 05:21:54 +0900

    update Reply type based on its number of children

--- lib/src/utils/reply_list_to_map.dart
+++ lib/src/utils/reply_list_to_map.dart
@@ -1,4 +1,4 @@
-import 'package:chat_bot_designer/src/models/reply.dart';
+import '../models/reply.dart';
 
 Map<Reply, Map<String, Reply>> repliesListToMap(List<Reply> replies) {
   return replies.fold({},
@@ -10,9 +10,11 @@ Map<Reply, Map<String, Reply>> repliesListToMap(List<Reply> replies) {
     Map<String, Reply> childMap = {};
     for (int i = 0; i < children.length; i++) {
       final childReply = children[i];
-      childMap[currentReply.choices[i].toString()] = childReply;
+      childMap[currentReply.choices[i].text] = childReply;
     }
 
+    currentReply.type =
+        (children.isEmpty) ? ReplyType.text : ReplyType.template;
     repliesMap[currentReply] = childMap;
 
     return repliesMap;
